79179796535ab6cb87d83b2d11a73442e929f985
RevK <github@k.gg>
2024-04-21 10:09:11 +0100

    Not building yet

---
+++ settings.c
@@ -0,0 +1,163 @@
+// Settings
+// Generated from:-
+// settings.def
+// components/ESP32-RevK/settings.def
+
+#include <stdint.h>
+#include "sdkconfig.h"
+#include "settings.h"
+revk_settings_bits_t revk_settings_bits={0};
+#define	str(s)	#s
+#define	quote(s)	str(s)
+revk_settings_t const revk_settings[]={
+ {.type=REVK_SETTINGS_UNSIGNED,.gpio=1,.name="eyeleft",.comment="Left eye",.group=1,.len=7,.dot=3,.ptr=&eyeleft,.size=sizeof(revk_gpio_t),.fix=1,.set=1,.flags="- ~↓↕⇕"},
+ {.type=REVK_SETTINGS_UNSIGNED,.gpio=1,.name="eyeright",.comment="Right eye",.group=1,.len=8,.dot=3,.ptr=&eyeright,.size=sizeof(revk_gpio_t),.fix=1,.set=1,.flags="- ~↓↕⇕"},
+ {.type=REVK_SETTINGS_UNSIGNED,.gpio=1,.name="visorpwr",.comment="Visor servo power",.group=2,.len=8,.dot=5,.ptr=&visorpwr,.size=sizeof(revk_gpio_t),.fix=1,.set=1,.flags="- ~↓↕⇕"},
+ {.type=REVK_SETTINGS_UNSIGNED,.gpio=1,.name="visorpwm",.comment="Visor service PWM",.group=2,.len=8,.dot=5,.ptr=&visorpwm,.size=sizeof(revk_gpio_t),.fix=1,.set=1,.flags="- ~↓↕⇕"},
+ {.type=REVK_SETTINGS_UNSIGNED,.gpio=1,.name="rgb",.comment="RGB LED chain",.len=3,.ptr=&rgb,.size=sizeof(revk_gpio_t),.fix=1,.set=1,.flags="- ~↓↕⇕"},
+#ifdef	CONFIG_REVK_SETTINGS_PASSWORD
+ {.type=REVK_SETTINGS_STRING,.name="password",.comment="Settings password (this is not sent securely so use with care on local networks you control)",.len=8,.ptr=&password,.malloc=1,.revk=1,.secret=1},
+#endif
+ {.type=REVK_SETTINGS_STRING,.name="hostname",.comment="Host name",.len=8,.ptr=&hostname,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="appname",.len=7,.dq=1,.def=quote(CONFIG_REVK_APPNAME),.ptr=&appname,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="otahost",.comment="OTA hostname",.group=3,.len=7,.dot=3,.dq=1,.def=quote(CONFIG_REVK_OTAHOST),.ptr=&otahost,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_UNSIGNED,.name="otadays",.comment="OTA auto load (days)",.group=3,.len=7,.dot=3,.dq=1,.def=quote(CONFIG_REVK_OTADAYS),.ptr=&otadays,.size=sizeof(uint8_t),.revk=1},
+ {.type=REVK_SETTINGS_UNSIGNED,.name="otastart",.comment="OTA check after startup (min seconds)",.group=3,.len=8,.dot=3,.def="600",.ptr=&otastart,.size=sizeof(uint16_t),.revk=1},
+ {.type=REVK_SETTINGS_BIT,.name="otaauto",.comment="OTA auto upgrade",.group=3,.len=7,.dot=3,.def="1",.bit=REVK_SETTINGS_BITFIELD_otaauto,.revk=1,.live=1},
+ {.type=REVK_SETTINGS_BIT,.name="otabeta",.comment="OTA from beta release",.group=3,.len=7,.dot=3,.bit=REVK_SETTINGS_BITFIELD_otabeta,.revk=1,.live=1},
+ {.type=REVK_SETTINGS_BLOB,.name="otacert",.comment="OTA cert of otahost",.group=3,.len=7,.dot=3,.dq=1,.def=quote(CONFIG_REVK_OTACERT),.ptr=&otacert,.malloc=1,.revk=1,.base64=1},
+ {.type=REVK_SETTINGS_STRING,.name="ntphost",.comment="NTP host",.len=7,.dq=1,.def=quote(CONFIG_REVK_NTPHOST),.ptr=&ntphost,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="tz",.comment="Timezone (<a href='https://gist.github.com/alwynallan/24d96091655391107939' target=_blank>info</a>)",.len=2,.dq=1,.def=quote(CONFIG_REVK_TZ),.ptr=&tz,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_UNSIGNED,.name="watchdogtime",.comment="Watchdog (seconds)",.len=12,.dq=1,.def=quote(CONFIG_REVK_WATCHDOG),.ptr=&watchdogtime,.size=sizeof(uint32_t),.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="prefixcommand",.comment="MQTT Prefix for commands (simple text, no / included)",.group=4,.len=13,.dot=6,.def="command",.ptr=&prefixcommand,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="prefixsetting",.comment="MQTT Prefix for settings (simple text, no / included)",.group=4,.len=13,.dot=6,.def="setting",.ptr=&prefixsetting,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="prefixstate",.comment="MQTT Prefix for state",.group=4,.len=11,.dot=6,.def="state",.ptr=&prefixstate,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="prefixevent",.comment="MQTT Prefix for event",.group=4,.len=11,.dot=6,.def="event",.ptr=&prefixevent,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="prefixinfo",.comment="MQTT Prefix for info",.group=4,.len=10,.dot=6,.def="info",.ptr=&prefixinfo,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="prefixerror",.comment="MQTT Prefix for error",.group=4,.len=11,.dot=6,.def="error",.ptr=&prefixerror,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_BIT,.name="prefixapp",.comment="MQTT includes appname",.group=4,.len=9,.dot=6,.dq=1,.def=quote(CONFIG_REVK_PREFIXAPP),.bit=REVK_SETTINGS_BITFIELD_prefixapp,.revk=1},
+#ifdef	CONFIG_REVK_BLINK_LIB
+ {.type=REVK_SETTINGS_UNSIGNED,.gpio=1,.name="blink",.comment="LED array",.len=5,.dq=1,.def=quote(CONFIG_REVK_BLINK),.ptr=&blink,.size=sizeof(revk_gpio_t),.fix=1,.set=1,.flags="- ~↓↕⇕",.revk=1,.array=3},
+#endif
+ {.type=REVK_SETTINGS_BLOB,.name="clientkey",.group=5,.len=9,.dot=6,.ptr=&clientkey,.malloc=1,.revk=1,.base64=1},
+ {.type=REVK_SETTINGS_BLOB,.name="clientcert",.group=5,.len=10,.dot=6,.ptr=&clientcert,.malloc=1,.revk=1,.base64=1},
+#ifdef  CONFIG_REVK_APMODE
+#ifdef	CONFIG_REVK_APCONFIG
+ {.type=REVK_SETTINGS_UNSIGNED,.name="apport",.comment="TCP port for config web pages on AP",.group=6,.len=6,.dot=2,.dq=1,.def=quote(CONFIG_REVK_APPORT),.ptr=&apport,.size=sizeof(uint16_t),.revk=1},
+#endif
+ {.type=REVK_SETTINGS_UNSIGNED,.name="aptime",.comment="Limit AP to time (seconds)",.group=6,.len=6,.dot=2,.dq=1,.def=quote(CONFIG_REVK_APTIME),.ptr=&aptime,.size=sizeof(uint32_t),.revk=1},
+ {.type=REVK_SETTINGS_UNSIGNED,.name="apwait",.comment="Wait off line before starting AP (seconds)",.group=6,.len=6,.dot=2,.dq=1,.def=quote(CONFIG_REVK_APWAIT),.ptr=&apwait,.size=sizeof(uint32_t),.revk=1},
+ {.type=REVK_SETTINGS_UNSIGNED,.gpio=1,.name="apgpio",.comment="Start AP on GPIO",.group=6,.len=6,.dot=2,.dq=1,.def=quote(CONFIG_REVK_APGPIO),.ptr=&apgpio,.size=sizeof(revk_gpio_t),.fix=1,.set=1,.flags="- ~↓↕⇕",.revk=1},
+#endif
+#ifdef  CONFIG_REVK_MQTT
+ {.type=REVK_SETTINGS_STRING,.name="mqtthost",.group=7,.len=8,.dot=4,.dq=1,.def=quote(CONFIG_REVK_MQTTHOST),.ptr=&mqtthost,.malloc=1,.revk=1,.array=CONFIG_REVK_MQTT_CLIENTS},
+ {.type=REVK_SETTINGS_UNSIGNED,.name="mqttport",.group=7,.len=8,.dot=4,.dq=1,.def=quote(CONFIG_REVK_MQTTPORT),.ptr=&mqttport,.size=sizeof(uint16_t),.revk=1,.array=CONFIG_REVK_MQTT_CLIENTS},
+ {.type=REVK_SETTINGS_STRING,.name="mqttuser",.group=7,.len=8,.dot=4,.dq=1,.def=quote(CONFIG_REVK_MQTTUSER),.ptr=&mqttuser,.malloc=1,.revk=1,.array=CONFIG_REVK_MQTT_CLIENTS},
+ {.type=REVK_SETTINGS_STRING,.name="mqttpass",.group=7,.len=8,.dot=4,.dq=1,.def=quote(CONFIG_REVK_MQTTPASS),.ptr=&mqttpass,.malloc=1,.revk=1,.array=CONFIG_REVK_MQTT_CLIENTS,.secret=1},
+ {.type=REVK_SETTINGS_BLOB,.name="mqttcert",.group=7,.len=8,.dot=4,.dq=1,.def=quote(CONFIG_REVK_MQTTCERT),.ptr=&mqttcert,.malloc=1,.revk=1,.array=CONFIG_REVK_MQTT_CLIENTS,.base64=1},
+#endif
+#if     defined(CONFIG_REVK_WIFI) || defined(CONFIG_REVK_MESH)
+ {.type=REVK_SETTINGS_UNSIGNED,.name="wifireset",.group=8,.len=9,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFIRESET),.ptr=&wifireset,.size=sizeof(uint16_t),.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="wifissid",.comment="WiFI SSID (name)",.group=8,.len=8,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFISSID),.ptr=&wifissid,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="wifipass",.comment="WiFi password",.group=8,.len=8,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFIPASS),.ptr=&wifipass,.malloc=1,.revk=1,.secret=1},
+ {.type=REVK_SETTINGS_STRING,.name="wifiip",.comment="WiFi Fixed IP",.group=8,.len=6,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFIIP),.ptr=&wifiip,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="wifigw",.comment="WiFi Fixed gateway",.group=8,.len=6,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFIGW),.ptr=&wifigw,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="wifidns",.comment="WiFi fixed DNS",.group=8,.len=7,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFIDNS),.ptr=&wifidns,.malloc=1,.revk=1,.array=3},
+ {.type=REVK_SETTINGS_OCTET,.name="wifibssid",.comment="WiFI BSSID",.group=8,.len=9,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFIBSSID),.ptr=&wifibssid,.size=sizeof(uint8_t[6]),.revk=1,.hex=1},
+ {.type=REVK_SETTINGS_UNSIGNED,.name="wifichan",.comment="WiFI channel",.group=8,.len=8,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFICHAN),.ptr=&wifichan,.size=sizeof(uint8_t),.revk=1},
+ {.type=REVK_SETTINGS_BIT,.name="wifips",.comment="WiFi power save",.group=8,.len=6,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFIPS),.bit=REVK_SETTINGS_BITFIELD_wifips,.revk=1},
+ {.type=REVK_SETTINGS_BIT,.name="wifimaxps",.comment="WiFi power save (max)",.group=8,.len=9,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFIMAXPS),.bit=REVK_SETTINGS_BITFIELD_wifimaxps,.revk=1},
+#endif
+#ifndef	CONFIG_REVK_MESH
+ {.type=REVK_SETTINGS_STRING,.name="apssid",.comment="AP mode SSID (name)",.group=6,.len=6,.dot=2,.dq=1,.def=quote(CONFIG_REVK_APSSID),.ptr=&apssid,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="appass",.comment="AP mode password",.group=6,.len=6,.dot=2,.dq=1,.def=quote(CONFIG_REVK_APPASS),.ptr=&appass,.malloc=1,.revk=1,.secret=1},
+ {.type=REVK_SETTINGS_UNSIGNED,.name="apmax",.group=6,.len=5,.dot=2,.dq=1,.def=quote(CONFIG_REVK_APMAX),.ptr=&apmax,.size=sizeof(uint8_t),.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="apip",.comment="AP mode block",.group=6,.len=4,.dot=2,.dq=1,.def=quote(CONFIG_REVK_APIP),.ptr=&apip,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_BIT,.name="aplr",.comment="AP LR mode",.group=6,.len=4,.dot=2,.dq=1,.def=quote(CONFIG_REVK_APLR),.bit=REVK_SETTINGS_BITFIELD_aplr,.revk=1},
+ {.type=REVK_SETTINGS_BIT,.name="aphide",.comment="AP hide SSID",.group=6,.len=6,.dot=2,.dq=1,.def=quote(CONFIG_REVK_APHIDE),.bit=REVK_SETTINGS_BITFIELD_aphide,.revk=1},
+#endif
+#ifdef	CONFIG_REVK_MESH
+ {.type=REVK_SETTINGS_STRING,.name="nodename",.len=8,.ptr=&nodename,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_UNSIGNED,.name="meshreset",.group=9,.len=9,.dot=4,.dq=1,.def=quote(CONFIG_REVK_MESHRESET),.ptr=&meshreset,.size=sizeof(uint16_t),.revk=1},
+ {.type=REVK_SETTINGS_OCTET,.name="meshid",.group=9,.len=6,.dot=4,.dq=1,.def=quote(CONFIG_REVK_MESHID),.ptr=&meshid,.size=sizeof(uint8_t[6]),.revk=1,.hex=1},
+ {.type=REVK_SETTINGS_OCTET,.name="meshkey",.group=9,.len=7,.dot=4,.ptr=&meshkey,.size=sizeof(uint8_t[16]),.revk=1,.secret=1,.hex=1},
+ {.type=REVK_SETTINGS_UNSIGNED,.name="meshwidth",.group=9,.len=9,.dot=4,.dq=1,.def=quote(CONFIG_REVK_MESHWIDTH),.ptr=&meshwidth,.size=sizeof(uint16_t),.revk=1},
+ {.type=REVK_SETTINGS_UNSIGNED,.name="meshdepth",.group=9,.len=9,.dot=4,.dq=1,.def=quote(CONFIG_REVK_MESHDEPTH),.ptr=&meshdepth,.size=sizeof(uint16_t),.revk=1},
+ {.type=REVK_SETTINGS_UNSIGNED,.name="meshmax",.group=9,.len=7,.dot=4,.dq=1,.def=quote(CONFIG_REVK_MESHMAX),.ptr=&meshmax,.size=sizeof(uint16_t),.revk=1},
+ {.type=REVK_SETTINGS_STRING,.name="meshpass",.group=9,.len=8,.dot=4,.dq=1,.def=quote(CONFIG_REVK_MESHPASS),.ptr=&meshpass,.malloc=1,.revk=1,.secret=1},
+ {.type=REVK_SETTINGS_BIT,.name="meshlr",.group=9,.len=6,.dot=4,.dq=1,.def=quote(CONFIG_REVK_MESHLR),.bit=REVK_SETTINGS_BITFIELD_meshlr,.revk=1},
+ {.type=REVK_SETTINGS_BIT,.name="meshroot",.group=9,.len=8,.dot=4,.bit=REVK_SETTINGS_BITFIELD_meshroot,.revk=1},
+#endif
+{0}};
+#undef quote
+#undef str
+revk_gpio_t eyeleft={0};
+revk_gpio_t eyeright={0};
+revk_gpio_t visorpwr={0};
+revk_gpio_t visorpwm={0};
+revk_gpio_t rgb={0};
+#ifdef	CONFIG_REVK_SETTINGS_PASSWORD
+char* password=NULL;
+#endif
+char* hostname=NULL;
+char* appname=NULL;
+char* otahost=NULL;
+uint8_t otadays=0;
+uint16_t otastart=0;
+revk_settings_blob_t* otacert=NULL;
+char* ntphost=NULL;
+char* tz=NULL;
+uint32_t watchdogtime=0;
+char* prefixcommand=NULL;
+char* prefixsetting=NULL;
+char* prefixstate=NULL;
+char* prefixevent=NULL;
+char* prefixinfo=NULL;
+char* prefixerror=NULL;
+#ifdef	CONFIG_REVK_BLINK_LIB
+revk_gpio_t blink[3]={0};
+#endif
+revk_settings_blob_t* clientkey=NULL;
+revk_settings_blob_t* clientcert=NULL;
+#ifdef  CONFIG_REVK_APMODE
+#ifdef	CONFIG_REVK_APCONFIG
+uint16_t apport=0;
+#endif
+uint32_t aptime=0;
+uint32_t apwait=0;
+revk_gpio_t apgpio={0};
+#endif
+#ifdef  CONFIG_REVK_MQTT
+char* mqtthost[CONFIG_REVK_MQTT_CLIENTS]={0};
+uint16_t mqttport[CONFIG_REVK_MQTT_CLIENTS]={0};
+char* mqttuser[CONFIG_REVK_MQTT_CLIENTS]={0};
+char* mqttpass[CONFIG_REVK_MQTT_CLIENTS]={0};
+revk_settings_blob_t* mqttcert[CONFIG_REVK_MQTT_CLIENTS]={0};
+#endif
+#if     defined(CONFIG_REVK_WIFI) || defined(CONFIG_REVK_MESH)
+uint16_t wifireset=0;
+char* wifissid=NULL;
+char* wifipass=NULL;
+char* wifiip=NULL;
+char* wifigw=NULL;
+char* wifidns[3]={0};
+uint8_t wifibssid[6]={0};
+uint8_t wifichan=0;
+#endif
+#ifndef	CONFIG_REVK_MESH
+char* apssid=NULL;
+char* appass=NULL;
+uint8_t apmax=0;
+char* apip=NULL;
+#endif
+#ifdef	CONFIG_REVK_MESH
+char* nodename=NULL;
+uint16_t meshreset=0;
+uint8_t meshid[6]={0};
+uint8_t meshkey[16]={0};
+uint16_t meshwidth=0;
+uint16_t meshdepth=0;
+uint16_t meshmax=0;
+char* meshpass=NULL;
+#endif
+const char revk_settings_secret[]="✶✶✶✶✶✶✶✶";
---
+++ settings.h
@@ -0,0 +1,201 @@
+// Settings
+// Generated from:-
+// settings.def
+// components/ESP32-RevK/settings.def
+
+#include <stdint.h>
+#include <stddef.h>
+typedef struct revk_settings_s revk_settings_t;
+struct revk_settings_s {
+ void *ptr;
+ const char name[16];
+ const char *def;
+ const char *flags;
+ const char *comment;
+ uint16_t size;
+ uint8_t group;
+ uint8_t bit;
+ uint8_t dot:4;
+ uint8_t len:4;
+ uint8_t type:3;
+ uint8_t decimal:5;
+ uint8_t array:7;
+ uint8_t malloc:1;
+ uint8_t revk:1;
+ uint8_t live:1;
+ uint8_t fix:1;
+ uint8_t set:1;
+ uint8_t hex:1;
+ uint8_t base64:1;
+ uint8_t secret:1;
+ uint8_t dq:1;
+ uint8_t gpio:1;
+ uint8_t rtc:1;
+};
+typedef struct revk_settings_blob_s revk_settings_blob_t;
+struct revk_settings_blob_s {
+ uint16_t len;
+ uint8_t data[];
+};
+typedef struct revk_gpio_s revk_gpio_t;
+struct revk_gpio_s {
+ uint16_t num:10;
+ uint16_t strong:1;
+ uint16_t weak:1;
+ uint16_t pulldown:1;
+ uint16_t nopull:1;
+ uint16_t invert:1;
+ uint16_t set:1;
+};
+enum {
+#ifdef	CONFIG_REVK_SETTINGS_PASSWORD
+#endif
+ REVK_SETTINGS_BITFIELD_otaauto,
+ REVK_SETTINGS_BITFIELD_otabeta,
+ REVK_SETTINGS_BITFIELD_prefixapp,
+#ifdef	CONFIG_REVK_BLINK_LIB
+#endif
+#ifdef  CONFIG_REVK_APMODE
+#ifdef	CONFIG_REVK_APCONFIG
+#endif
+#endif
+#ifdef  CONFIG_REVK_MQTT
+#endif
+#if     defined(CONFIG_REVK_WIFI) || defined(CONFIG_REVK_MESH)
+ REVK_SETTINGS_BITFIELD_wifips,
+ REVK_SETTINGS_BITFIELD_wifimaxps,
+#endif
+#ifndef	CONFIG_REVK_MESH
+ REVK_SETTINGS_BITFIELD_aplr,
+ REVK_SETTINGS_BITFIELD_aphide,
+#endif
+#ifdef	CONFIG_REVK_MESH
+ REVK_SETTINGS_BITFIELD_meshlr,
+ REVK_SETTINGS_BITFIELD_meshroot,
+#endif
+};
+typedef struct revk_settings_bits_s revk_settings_bits_t;
+struct revk_settings_bits_s {
+#ifdef	CONFIG_REVK_SETTINGS_PASSWORD
+#endif
+ uint8_t otaauto:1;	// OTA auto upgrade
+ uint8_t otabeta:1;	// OTA from beta release
+ uint8_t prefixapp:1;	// MQTT includes appname
+#ifdef	CONFIG_REVK_BLINK_LIB
+#endif
+#ifdef  CONFIG_REVK_APMODE
+#ifdef	CONFIG_REVK_APCONFIG
+#endif
+#endif
+#ifdef  CONFIG_REVK_MQTT
+#endif
+#if     defined(CONFIG_REVK_WIFI) || defined(CONFIG_REVK_MESH)
+ uint8_t wifips:1;	// WiFi power save
+ uint8_t wifimaxps:1;	// WiFi power save (max)
+#endif
+#ifndef	CONFIG_REVK_MESH
+ uint8_t aplr:1;	// AP LR mode
+ uint8_t aphide:1;	// AP hide SSID
+#endif
+#ifdef	CONFIG_REVK_MESH
+ uint8_t meshlr:1;
+ uint8_t meshroot:1;
+#endif
+};
+extern revk_gpio_t eyeleft;	// Left eye
+extern revk_gpio_t eyeright;	// Right eye
+extern revk_gpio_t visorpwr;	// Visor servo power
+extern revk_gpio_t visorpwm;	// Visor service PWM
+extern revk_gpio_t rgb;	// RGB LED chain
+#ifdef	CONFIG_REVK_SETTINGS_PASSWORD
+extern char* password;	// Settings password (this is not sent securely so use with care on local networks you control)
+#endif
+extern char* hostname;	// Host name
+extern char* appname;
+extern char* otahost;	// OTA hostname
+extern uint8_t otadays;	// OTA auto load (days)
+extern uint16_t otastart;	// OTA check after startup (min seconds)
+#define	otaauto	revk_settings_bits.otaauto
+#define	otabeta	revk_settings_bits.otabeta
+extern revk_settings_blob_t* otacert;	// OTA cert of otahost
+extern char* ntphost;	// NTP host
+extern char* tz;	// Timezone (<a href='https://gist.github.com/alwynallan/24d96091655391107939' target=_blank>info</a>)
+extern uint32_t watchdogtime;	// Watchdog (seconds)
+extern char* prefixcommand;	// MQTT Prefix for commands (simple text, no / included)
+extern char* prefixsetting;	// MQTT Prefix for settings (simple text, no / included)
+extern char* prefixstate;	// MQTT Prefix for state
+extern char* prefixevent;	// MQTT Prefix for event
+extern char* prefixinfo;	// MQTT Prefix for info
+extern char* prefixerror;	// MQTT Prefix for error
+#define	prefixapp	revk_settings_bits.prefixapp
+#ifdef	CONFIG_REVK_BLINK_LIB
+extern revk_gpio_t blink[3];	// LED array
+#endif
+extern revk_settings_blob_t* clientkey;
+extern revk_settings_blob_t* clientcert;
+#ifdef  CONFIG_REVK_APMODE
+#ifdef	CONFIG_REVK_APCONFIG
+extern uint16_t apport;	// TCP port for config web pages on AP
+#endif
+extern uint32_t aptime;	// Limit AP to time (seconds)
+extern uint32_t apwait;	// Wait off line before starting AP (seconds)
+extern revk_gpio_t apgpio;	// Start AP on GPIO
+#endif
+#ifdef  CONFIG_REVK_MQTT
+extern char* mqtthost[CONFIG_REVK_MQTT_CLIENTS];
+extern uint16_t mqttport[CONFIG_REVK_MQTT_CLIENTS];
+extern char* mqttuser[CONFIG_REVK_MQTT_CLIENTS];
+extern char* mqttpass[CONFIG_REVK_MQTT_CLIENTS];
+extern revk_settings_blob_t* mqttcert[CONFIG_REVK_MQTT_CLIENTS];
+#endif
+#if     defined(CONFIG_REVK_WIFI) || defined(CONFIG_REVK_MESH)
+extern uint16_t wifireset;
+extern char* wifissid;	// WiFI SSID (name)
+extern char* wifipass;	// WiFi password
+extern char* wifiip;	// WiFi Fixed IP
+extern char* wifigw;	// WiFi Fixed gateway
+extern char* wifidns[3];	// WiFi fixed DNS
+extern uint8_t wifibssid[6];	// WiFI BSSID
+extern uint8_t wifichan;	// WiFI channel
+#define	wifips	revk_settings_bits.wifips
+#define	wifimaxps	revk_settings_bits.wifimaxps
+#endif
+#ifndef	CONFIG_REVK_MESH
+extern char* apssid;	// AP mode SSID (name)
+extern char* appass;	// AP mode password
+extern uint8_t apmax;
+extern char* apip;	// AP mode block
+#define	aplr	revk_settings_bits.aplr
+#define	aphide	revk_settings_bits.aphide
+#endif
+#ifdef	CONFIG_REVK_MESH
+extern char* nodename;
+extern uint16_t meshreset;
+extern uint8_t meshid[6];
+extern uint8_t meshkey[16];
+extern uint16_t meshwidth;
+extern uint16_t meshdepth;
+extern uint16_t meshmax;
+extern char* meshpass;
+#define	meshlr	revk_settings_bits.meshlr
+#define	meshroot	revk_settings_bits.meshroot
+#endif
+extern revk_settings_bits_t revk_settings_bits;
+enum {
+ REVK_SETTINGS_UNSIGNED,
+ REVK_SETTINGS_BIT,
+ REVK_SETTINGS_BLOB,
+ REVK_SETTINGS_STRING,
+ REVK_SETTINGS_OCTET,
+};
+#define	REVK_SETTINGS_HAS_COMMENT
+#define	REVK_SETTINGS_HAS_GPIO
+#define	REVK_SETTINGS_HAS_NUMERIC
+#define	REVK_SETTINGS_HAS_UNSIGNED
+#define	REVK_SETTINGS_HAS_BIT
+#define	REVK_SETTINGS_HAS_BLOB
+#define	REVK_SETTINGS_HAS_STRING
+#define	REVK_SETTINGS_HAS_OCTET
+typedef uint8_t revk_setting_bits_t[8];
+typedef uint8_t revk_setting_group_t[2];
+extern const char revk_settings_secret[];
